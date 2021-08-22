9027ed988c179899e89624d3354ca05e4dcf9b8b
Paul-DS <paul.david.sivelle@gmail.com>
2021-08-22 09:46:21 +0200

    Fix the python3-requests dependency

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -9,7 +9,7 @@ echo "*			      Installing dependencies		    	 *"
 echo "********************************************************"
 sudo apt-get update
 echo 30 > ${PROGRESS_FILE}
-sudo apt-get install -y python3 python3-pip python-requests
+sudo apt-get install -y python3 python3-pip python3-requests
 echo 60 > ${PROGRESS_FILE}
 sudo pip3 install requests setuptools pyudev
 echo 100 > ${PROGRESS_FILE}
